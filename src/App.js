76d7646f322f710ec374cd3a8c7f9d5f63e30acb
lijun <443848406@qq.com>
2019-09-20 17:11:06 +0800

    后台管理系统框架

--- src/App.js
+++ src/App.js
@@ -1,68 +1,13 @@
 import React from 'react';
 
-import { Layout, Menu, Breadcrumb, Icon } from 'antd';
-
-import './App.css'
-
-const { Header, Content, Footer, Sider } = Layout;
-const { SubMenu } = Menu;
+import Home from './components/Home'
 
 class App extends React.Component{
 
-  state = {
-    collapsed: false,
-  };
-
-  onCollapse = collapsed => {
-    console.log(collapsed);
-    this.setState({ collapsed });
-  };
-
-  render() {
-    return (
-      <Layout style={{ minHeight: '100vh' }}>
-        <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
-          <div className="logo" />
-          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-            <SubMenu
-              key="sub1"
-              title={
-                <span>
-                  <Icon type="user" />
-                  <span>用户管理</span>
-                </span>
-              }>
-              <Menu.Item key="3">用户列表</Menu.Item>
-              <Menu.Item key="4">添加用户</Menu.Item>
-            </SubMenu>
-            <SubMenu
-              key="sub2"
-              title={
-                <span>
-                  <Icon type="file" />
-                  <span>订单管理</span>
-                </span>
-              }
-            >
-              <Menu.Item key="6">订单列表</Menu.Item>
-            </SubMenu>
-          </Menu>
-        </Sider>
-        <Layout>
-          <Header style={{ background: '#001529', padding: 0 }}>
-            <h1 style={{ textAlign: "center" }}>用户后台管理系统</h1>
-          </Header>
-          <Content style={{ margin: '0 16px' }}>
-            <Breadcrumb style={{ margin: '16px 0' }}>
-              <Breadcrumb.Item>User</Breadcrumb.Item>
-              <Breadcrumb.Item>Bill</Breadcrumb.Item>
-            </Breadcrumb>
-            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>Bill is a cat.</div>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-        </Layout>
-      </Layout>
-    );
+  render(){
+    return <div>
+               <Home/>
+           </div>
   }
 }
 
---
+++ src/components/TabLsit/UrseLsit.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import { Table, Button } from 'antd';
+
+const columns = [
+    {
+      title: '#',
+      dataIndex: 'index',
+      key: 'index',
+      render: idx => {idx+1},
+    },
+    {
+      title: '代办事项',
+      dataIndex: 'content',
+      key: 'content',
+    },
+    {
+      title: '预计完成时间',
+      dataIndex: 'date',
+      key: 'date',
+    },
+    {
+      title: '是否完成',
+      key: 'done',
+      dataIndex: 'done',
+    },
+    {
+      title: '操作',
+      key: 'actions',
+      render: () => (
+        <div>
+            <Button>点击</Button>
+        </div>
+      ),
+    },
+  ];
+
+  const data = []
+
+class UrseLsit extends React.Component{
+    render(){
+        return <div>
+                   <Table columns={columns} dataSource={data} />
+               </div>
+    }
+}
+
+export default UrseLsit
+
+
+
