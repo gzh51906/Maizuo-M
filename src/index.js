4280ee38435422945806fe62fd99fe92fb298882
nsg <2643198646@qq.com>
2019-09-21 16:32:40 +0800

    完成登录静态页面与验证功能--bingqun

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { render } from 'react-dom';
+import { HashRouter } from 'react-router-dom';
 
-import {render} from 'react-dom';
 
 import App from './App';
 
 
-render(
-    <App/>,
 
-    document.getElementById('app')
+render(
+
+    <HashRouter>
+        <App />
+    </HashRouter>
+    ,
+    document.querySelector('#app')
 )
